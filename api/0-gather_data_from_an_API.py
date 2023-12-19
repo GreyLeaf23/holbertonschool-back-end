5efdd189295cea89e0f1820900d05b909b235c06
GreyLeaf23 <christian.torres2894@gmail.com>
2023-12-19 10:14:07 -0400

    Module returns employee info of their IDs and given completed tasks.

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,8 +3,6 @@
 Module returns information about his/her TO_DO list progress
 when an employee ID is provided as argument.
 """
-import urllib
-import json
 import requests
 import sys
 
@@ -16,21 +14,23 @@ def get_employee_todo_progress(employee_id):
     # Get the user name through the API
     user_response = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-    user_data = user_response.json() # Parse user names to JSON
+    user_data = user_response.json()  # Parse user names to JSON
     employee_name = user_data['name']
 
 
-    # Get the user's tasks through the API
+#  Get the user's tasks through the API
     todo_response = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
             employee_id))
-    todo_data = todo_response.json() # Parse user's tasks to JSON
-    total_tasks = len(todo_data) # Get the total number of tasks
-    
-    # Get the number of completed tasks
-    done_tasks = [task for task in todo_data if task['completed'] is True]
-    num_done_tasks = len(done_tasks) # Get the number of completed tasks
+    todo_data = todo_response.json()  # Parse user's tasks to JSON
+    total_tasks = len(todo_data)  # Get the total number of tasks
 
+
+# Get the number of completed tasks
+    done_tasks = [task for task in todo_data if task['completed'] is True]
+    num_done_tasks = len(done_tasks)  # Get  the number of completed tasks
+
+# Print the user's tasks
     print("Employee {} is done with tasks({}/{}):".format(
         employee_name, num_done_tasks, total_tasks))
     for task in done_tasks:
@@ -40,4 +40,3 @@ def get_employee_todo_progress(employee_id):
 if __name__ == '__main__':
     employee_id = int(sys.argv[1])
     get_employee_todo_progress(employee_id)
-
